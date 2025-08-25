1892109979f4dc53a31a8058782f8c35a38699cc
Steven Bailey <stevenbailey@MacBookAir.attlocal.net>
2025-08-25 10:04:29 -0400

    upgrading to support ubuntu noble [skip ci]

--- .eleventy.js
+++ .eleventy.js
@@ -1,57 +1,41 @@
-// To inspect
-const inspect = require("util").inspect;
+// eleventy.config.js (Eleventy v3, ESM)
+import { minify } from "html-minifier-terser";
 
-// To minify HTML
-const htmlmin = require("html-minifier");
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "./src/robots.txt": "robots.txt" });
+  eleventyConfig.addPassthroughCopy({ "./src/images": "images" });
+  eleventyConfig.addPassthroughCopy({ "./src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "./src/fonts": "css/fonts" });
 
+  eleventyConfig.addWatchTarget("./src/css");
 
-module.exports = function (eleventyConfig) {
+  eleventyConfig.addCollection("orderedPages", (collection) =>
+    collection.getFilteredByTag("pages").sort((a, b) => (a.data.order ?? 0) - (b.data.order ?? 0))
+  );
 
-  // Watch for sass file changes
-  eleventyConfig.addWatchTarget("./src/sass/");
-
-  // Let some files pass through to public
-  eleventyConfig.addPassthroughCopy("./src/robots.txt");
-  eleventyConfig.addPassthroughCopy("./src/images");
-  eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addPassthroughCopy({"./src/fonts" : "css/fonts/"});
-
-  // Allow for inspection
-  eleventyConfig.addFilter("debug", (content) => `${inspect(content)}`);
-
-  // Helper to sort pages collection by frontmatter "order"
-  eleventyConfig.addCollection("orderedPages", function (collection) {
-    return collection.getFilteredByTag("pages").sort((a, b) => {
-      return a.data.order - b.data.order;
-    });
-  });
-
-  // Minify HTML Output
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-    if( outputPath && outputPath.endsWith(".html") ) {
-      let minified = htmlmin.minify(content, {
+  eleventyConfig.addTransform("htmlmin", async function (content) {
+    if (this.outputPath && this.outputPath.endsWith(".html")) {
+      return await minify(content, {
         useShortDoctype: true,
         removeComments: true,
-        collapseWhitespace: true
+        collapseWhitespace: true,
+        keepClosingSlash: true,
+        minifyCSS: true,
+        minifyJS: true,
       });
-      return minified;
     }
     return content;
   });
 
+  eleventyConfig.setServerOptions({
+    showAllHosts: true,
+    domDiff: true,
+  });
+
   return {
-    addPassthroughCopy: true,
     templateFormats: ["njk", "html"],
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about it.
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for URLs (it does not affect your file structure)
-    pathPrefix: "/",
     htmlTemplateEngine: "njk",
-    dir: {
-      input: "src",
-      output: "public",
-    },
+    pathPrefix: "/",
+    dir: { input: "src", output: "public" },
   };
-};
+}
